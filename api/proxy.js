34656b6ba1d49eb4a39b30236fd4f7ec45dc09c9
Sebastián Lisandro Colque <122639349+SebaColque@users.noreply.github.com>
2025-09-17 09:56:14 -0300

    URL de participantes cambiada

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 // /api/proxy.js
 export default async function handler(req, res) {
-  const POWER_AUTOMATE_URL = "https://defaulte50a768fa1f54782902f745f77d641.64.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/ae29634cb8fd42b5a8b85fbce5f853bc/triggers/manual/paths/invoke?api-version=1"; // tu URL de Power Automate
+  const POWER_AUTOMATE_URL = "https://defaulte50a768fa1f54782902f745f77d641.64.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/ae29634cb8fd42b5a8b85fbce5f853bc/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6v8CNhQVtWc-JahByVUvyiUva65y_x-iy0xS5cqKC3M"; // tu URL de Power Automate
 
   try {
     const response = await fetch(POWER_AUTOMATE_URL);
